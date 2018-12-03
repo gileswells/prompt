d150e1712f8ecea0c9ade4437593130a39d1f5a8
Giles Wells <giles@mediagearhead.com>
2018-12-03 00:43:59 -0500

    Complete code coverage for BenchmarkService::calculateResults

--- tests/Unit/Services/BenchmarkServiceTest.php
+++ tests/Unit/Services/BenchmarkServiceTest.php
@@ -8,7 +8,17 @@ use App\Services\BenchmarkService;
 class BenchmarkServiceTest extends TestCase
 {
     /**
-     * A basic test example.
+     * @dataProvider calculateResultsDataProvider
+     */
+    public function testCalculateResults($expected, $results, $sort, $order)
+    {
+        $actual = BenchmarkService::calculateResults($results, $sort, $order);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Make sure that bad data throws the expected exceptions.
      *
      * @dataProvider calculateResultsExceptionsDataProvider
      *
@@ -25,6 +35,143 @@ class BenchmarkServiceTest extends TestCase
         ];
 
         BenchmarkService::calculateResults($dummy, $sort, $order);
+    }
+
+    /**
+     * Data Provider for sorting of results
+     *
+     * @return array
+     */
+    public function calculateResultsDataProvider()
+    {
+        return [
+            'empty-data' => [
+                [],
+                [],
+                null,
+                null,
+            ],
+            'average' => [
+                [
+                    'quickSort' => [
+                        'min' => 2,
+                        'max' => 4,
+                        'average' => 3,
+                    ],
+                    'bubbleSort' => [
+                        'min' => 1,
+                        'max' => 6,
+                        'average' => 4,
+                    ],
+                ],
+                [
+                    'bubbleSort' => [1, 6, 5],
+                    'quickSort' => [2, 4, 3, 3],
+                ],
+                'average',
+                'asc',
+            ],
+            'average-descending' => [
+                [
+                    'bubbleSort' => [
+                        'min' => 1,
+                        'max' => 6,
+                        'average' => 4,
+                    ],
+                    'quickSort' => [
+                        'min' => 2,
+                        'max' => 4,
+                        'average' => 3,
+                    ],
+                ],
+                [
+                    'bubbleSort' => [1, 6, 5],
+                    'quickSort' => [2, 4, 3, 3],
+                ],
+                'average',
+                'desc',
+            ],
+            'min' => [
+                [
+                    'bubbleSort' => [
+                        'min' => 1,
+                        'max' => 6,
+                        'average' => 4,
+                    ],
+                    'quickSort' => [
+                        'min' => 2,
+                        'max' => 4,
+                        'average' => 3,
+                    ],
+                ],
+                [
+                    'bubbleSort' => [1, 6, 5],
+                    'quickSort' => [2, 4, 3, 3],
+                ],
+                'min',
+                'asc',
+            ],
+            'min-descending' => [
+                [
+                    'quickSort' => [
+                        'min' => 2,
+                        'max' => 4,
+                        'average' => 3,
+                    ],
+                    'bubbleSort' => [
+                        'min' => 1,
+                        'max' => 6,
+                        'average' => 4,
+                    ],
+                ],
+                [
+                    'bubbleSort' => [1, 6, 5],
+                    'quickSort' => [2, 4, 3, 3],
+                ],
+                'min',
+                'desc',
+            ],
+            'max' => [
+                [
+                    'quickSort' => [
+                        'min' => 2,
+                        'max' => 4,
+                        'average' => 3,
+                    ],
+                    'bubbleSort' => [
+                        'min' => 1,
+                        'max' => 6,
+                        'average' => 4,
+                    ],
+                ],
+                [
+                    'bubbleSort' => [1, 6, 5],
+                    'quickSort' => [2, 4, 3, 3],
+                ],
+                'max',
+                'asc',
+            ],
+            'max-descending' => [
+                [
+                    'bubbleSort' => [
+                        'min' => 1,
+                        'max' => 6,
+                        'average' => 4,
+                    ],
+                    'quickSort' => [
+                        'min' => 2,
+                        'max' => 4,
+                        'average' => 3,
+                    ],
+                ],
+                [
+                    'bubbleSort' => [1, 6, 5],
+                    'quickSort' => [2, 4, 3, 3],
+                ],
+                'max',
+                'desc',
+            ],
+        ];
     }
 
     /**
