d47e304f468ffdc1c8ef509459fc6d454772102b
Giles Wells <giles@mediagearhead.com>
2018-12-02 23:20:19 -0500

    First verson of calculating results

--- app/Services/BenchmarkService.php
+++ app/Services/BenchmarkService.php
@@ -4,25 +4,50 @@ namespace App\Services;
 
 class BenchmarkService {
 
-	/**
-	 * @param  \Callable $function Function to be tested
-	 * @param  string    $name     Human readable function name
-	 * @param  integer   $count    Number of times to test
-	 *
-	 * @return array
-	 */
-	public static function run(callable $function, $params, $name, $count)
-	{
-		$output = [];
+    /**
+     * Process the requested function the requested number of times
+     *
+     * @param  \Callable $function Function to be tested
+     * @param  integer   $count    Number of times to test
+     *
+     * @return array
+     */
+    public static function run(callable $function, $params, $count)
+    {
+        $output = [];
+        for ($i = 0; $i < $count; $i ++) {
+            $start = microtime(true);
+            $function($params);
+            $end = microtime(true);
 
-		for ($i=0; $i < $count; $i++) {
-			$start = microtime(true);
-			$function($params);
-			$end = microtime(true);
+            $output[] = ($end - $start) * 1000;
+        }
 
-			$output[] = ($end - $start) * 1000;
-		}
+        return $output;
+    }
 
-		return $output;
-	}
+    /**
+     * Take in the benchmark raw data and format the data in a more logical format
+     *
+     * @param  array $result Raw result data from to be calculated and compared
+     *
+     * @return [type]
+     */
+    public static function calculateResults(array $results, $sort = 'average', $direction = 'asc')
+    {
+        if (empty($results)) {
+            return [];
+        }
+
+        $calculated = [];
+        foreach ($results as $key => $values) {
+            $calculated[$key] = [
+                'min' => min($values),
+                'max' => max($values),
+                'average' => array_sum($values) / count($values),
+            ];
+        }
+
+        return $calculated;
+    }
 }
